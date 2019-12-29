f059a6ca78ad5e65e5db58536e18f00b058acbde
IN\lalan.singh <lalan.singh@tavant.com>
2019-12-29 14:53:03 +0530

    code added

--- src/app/school/bulk-sms/bulk-sms.component.ts
+++ src/app/school/bulk-sms/bulk-sms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'app-bulk-sms',
@@ -6,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bulk-sms.component.scss']
 })
 export class BulkSmsComponent implements OnInit {
-
+  public columnDefs = [
+    { headerName: 'Registration #', width: '300', field: 'registration', sortable: true, filter: true, checkboxSelection: true, headerCheckboxSelection: true, headerCheckboxSelectionFilteredOnly: true },
+    { headerName: 'Student Name', width: '305', field: 'name', sortable: true, filter: true },
+    { headerName: 'Roll Number', width: '300', field: 'roll', sortable: true, filter: true },
+    { headerName: 'Mobile Number', width: '300', field: 'mobile', sortable: true, filter: true },
+    { headerName: 'Status', width: '300', field: 'status', sortable: true, filter: true }
+  ];
+  public rowData = [
+    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Acive' },
+    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Acive' },
+    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Acive' },
+    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Acive' },
+    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Acive' },
+    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Acive' },
+    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Acive' },
+    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Acive' }
+  ];
   constructor() { }
 
   ngOnInit() {
