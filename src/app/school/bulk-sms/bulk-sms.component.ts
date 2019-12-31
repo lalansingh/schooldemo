fbe6c0514bf395c76ca6693b119ac485ef20f038
IN\lalan.singh <lalan.singh@tavant.com>
2019-12-31 21:41:00 +0530

    code added

--- src/app/school/bulk-sms/bulk-sms.component.ts
+++ src/app/school/bulk-sms/bulk-sms.component.ts
@@ -8,21 +8,21 @@ import { ColDef } from 'ag-grid-community';
 })
 export class BulkSmsComponent implements OnInit {
   public columnDefs = [
-    { headerName: 'Registration #', width: '300', field: 'registration', sortable: true, filter: true, checkboxSelection: true, headerCheckboxSelection: true, headerCheckboxSelectionFilteredOnly: true },
-    { headerName: 'Student Name', width: '305', field: 'name', sortable: true, filter: true },
-    { headerName: 'Roll Number', width: '300', field: 'roll', sortable: true, filter: true },
+    { headerName: 'Registration #', width: '250', field: 'registration', sortable: true, filter: true, checkboxSelection: true, headerCheckboxSelection: true, headerCheckboxSelectionFilteredOnly: true },
+    { headerName: 'Student Name', width: '300', field: 'name', sortable: true, filter: true },
+    { headerName: 'Roll Number', width: '250', field: 'roll', sortable: true, filter: true },
     { headerName: 'Mobile Number', width: '300', field: 'mobile', sortable: true, filter: true },
     { headerName: 'Status', width: '300', field: 'status', sortable: true, filter: true }
   ];
   public rowData = [
-    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Acive' },
-    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Acive' },
-    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Acive' },
-    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Acive' },
-    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Acive' },
-    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Acive' },
-    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Acive' },
-    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Acive' }
+    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Sent' },
+    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Sent' },
+    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Pendding' },
+    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Diliverd' },
+    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Failed' },
+    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Failed' },
+    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Send' },
+    { name: 'John', registration: 'RG-009-02-2019', roll: 123456, mobile: '898767654', status: 'Pending' }
   ];
   constructor() { }
 
